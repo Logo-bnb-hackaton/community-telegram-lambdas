df051592123e97e840e3a9d23b186d0a0beaab5d
ilyamikheev <mikheev.show@gmail.com>
2023-04-24 15:45:32 +0300

    add websocket listeners

---
+++ websocket/subscription.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const ethers = require("ethers");
+const subscriptionContractABI = require("../abis/subscription.json")
+const subscriptionContractAddress = "0xe56e5FD2D7aeAde39B04EFb41992a233948D304e";
+const networkRpc = "https://data-seed-prebsc-1-s3.binance.org:8545";
+
+const startListenEventsFromSubscriptionContract = async () => {
+    
+    
+    let provider = new ethers.providers.JsonRpcProvider(networkRpc);
+    
+    let contract = new ethers.Contract(subscriptionContractAddress, subscriptionContractABI, provider);
+
+    contract.on("NewSubscription", (from, to, value, event) => {
+        console.log(`New subscription event received from ${from}`)
+        console.log(`New ${event}`);
+    });
+}
+
+startListenEventsFromSubscriptionContract()
