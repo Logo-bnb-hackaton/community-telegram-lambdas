347fc4b5851f04c90f8b778ddaf20652f60c24f2
ilyamikheev <mikheev.show@gmail.com>
2023-04-24 05:06:07 +0300

    fix bind handler

--- bind.js
+++ bind.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const AWS = require('aws-sdk');
-const { BINDING_TYPE, COMMON_ERROR_MESSAGE, unixTimestamp } = require('./common');
+const { BINDING_TYPE, COMMON_ERROR_MESSAGE, unixTimestamp, SUCCESS, telegramTable, ERROR } = require('./common');
 
 const region = "us-east-1";
 const telegramTableName = "Community-telegram";
@@ -9,17 +9,53 @@ const dynamo = new AWS.DynamoDB.DocumentClient({ region: region });
 
 
 module.exports.bind = async (event, context) => {
-    
+
     let body = JSON.parse(event.body);
     let { address, code, content_id } = body;
 
-    const binding = await getBindingByCode(code);
+    const result = await getBindingByCode(code);
 
-    binding.address = address;
-    binding.content_id = content_id;
-    binding.updated_at = unixTimestamp();
+    if (result.status === SUCCESS && result.item) {
 
+        const binding = result.item;
 
+        if (binding.content_id) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ message: "This code already used" })
+            }
+        }
+
+        binding.address = address;
+        binding.content_id = content_id;
+        binding.updated_at = unixTimestamp();
+
+        const saved = await saveBinding(binding);
+        
+        if (saved.status === SUCCESS) {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ message: "Successfuly binded" })
+            }
+        } else {
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ message: COMMON_ERROR_MESSAGE })
+            }
+        }
+    }
+
+    if (result.status === SUCCESS) {
+        return {
+            statusCode: 404,
+            body: JSON.stringify({ message: "Not found" })
+        }
+    }
+
+    return {
+        statusCode: 500,
+        body: JSON.stringify({ message: COMMON_ERROR_MESSAGE })
+    }
 }
 
 const getBindingByCode = async (code) => {
@@ -43,6 +79,26 @@ const getBindingByCode = async (code) => {
         return {
             status: "error",
             message: COMMON_ERROR_MESSAGE,
+        }
+    }
+}
+
+const saveBinding = async(binding) => {
+    
+    const params = {
+        TableName: telegramTable,
+        Item: binding
+    }
+    
+    try {
+        await dynamo.put(params).promise();
+        return {
+            status: SUCCESS
+        }
+    } catch (error) {
+        console.log(`Error when save binding with params: ${params}`, error);
+        return {
+            status: ERROR
         }
     }
 }
